1a3279be728c711fcfa7ccf990fe486225381388
A.N <spidernet12@gmail.com>
2023-01-08 09:13:36 +0300

    added test page

--- controllers/pageRandomPicture.js
+++ controllers/pageRandomPicture.js
@@ -1,6 +1,14 @@
+const { picModel } = require('../models/picture');
+
 module.exports = async (req, res, next) => {
+
+    const randomPic = await picModel.aggregate([
+        { $sample: { size: 1 } }
+    ]);
+
     res.render("random-picture", {
         user: req.user || null,
+        picture: randomPic[0],
         layout: "./layouts/default-layout",
     });
 }
